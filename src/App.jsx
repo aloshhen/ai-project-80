4067a80578a273fd4d1ea651c4205106868936bf
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-01-11 19:41:18 +0100

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navigation from './components/Navigation'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import About from './components/About'
+import Pricing from './components/Pricing'
+import CTA from './components/CTA'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Hero />
+      <Features />
+      <About />
+      <Pricing />
+      <CTA />
+      <Footer />
+    </>
+  )
+}
+
+export default App
